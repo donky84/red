6d808538cd485e99d4ab8a7e24b0b57cc4a2015c
Davide Pluda <donky84@gmail.com>
2017-05-05 16:40:46 -0700

    datastructure creation

---
+++ picker.py
@@ -0,0 +1,20 @@
+
+1,Nouns,1,Common Nouns,1,0.7
+1,Nouns,1,Common Nouns,2,0.6
+1,Nouns,2,Abstract Nouns,3,0.8
+1,Nouns,3,Proper Nouns,4,0.2
+1,Nouns,3,Proper Nouns,5,0.5
+1,Nouns,3,Proper Nouns,6,0.4
+
+2,Verbs,4,Action Verbs,7,0.9
+2,Verbs,4,Action Verbs,8,0.1
+
+2,Verbs,5,Transitive Verbs,9,0.3
+2,Verbs,5,Transitive Verbs,10,0.6
+2,Verbs,5,Transitive Verbs,11,0.4
+
+2,Verbs,6,Reflexive Verbs,12,0.2
+
+
+questions = {1: {1: {1: 0.7, 2: 0.6}, 2: {3: 0.8} , 3: {4: 0.2, 5: 0.5, 6: 0.4}},2: {4: {7: 0.9, 8: 0.1}, 5: {9: 0.3, 10: 0.6, 11:0.4}, 6: {12:0.2}}} 
+
